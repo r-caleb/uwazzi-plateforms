acb17770470bac27356b716d4fe6b13615582592
hermanngumbu <https://github.com/hermanngumbu.com>
2023-03-14 11:49:36 +0100

    collaps feature

--- src/screens/TextesLegaux/TextesLegauxScreen.js
+++ src/screens/TextesLegaux/TextesLegauxScreen.js
@@ -25,7 +25,7 @@ function TextesScreen() {
          aliquip ex ea commodo consequat. Duis aute irure dolor in
          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-         culpa qui officia deserunt mollit anim id est laborum.
+         culpa qui officia deserunt mollit anim id est laborum.nt occaecat cupidatat non proident, 
        </Accordion.Body>
      </Accordion.Item>
      <Accordion.Item eventKey="1">
